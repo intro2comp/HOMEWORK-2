77069873011665958ce65b320e0b1a3736ad4a7f
Ted Moseley <tmoseley1106@gmail.com>
2019-03-29 10:52:23 -0400

    Let problem 2 threads run at the same time

--- problem_2/fib.c
+++ problem_2/fib.c
@@ -38,10 +38,17 @@ void *thread_print_fib(void *arg_struct)
 
   pthread_create(&child, NULL, thread_calc_fib, args);
 
-  pthread_join(child, NULL);
-
   for (int i = 0; i < args->n; i++)
-    printf("%d: %d\n", i + 1, args->arr[i]);
+    while (1) {
+      int val = args->arr[i];
+
+      if (val != -1) {  // If a value has been calculated for this index...
+        printf("%d: %d\n", i + 1, val);
+        break;  // Exit the loop
+      }
+    }  // Move on to the next index
+
+  pthread_join(child, NULL);
 
   pthread_exit(0);
 }
@@ -61,6 +68,10 @@ int main(int argc, char *argv[])
 
   parent_args.n = count;
   parent_args.arr = malloc(sizeof(int) * count);
+
+  // Set all values to -1 to act as NULL
+  for (int i = 0; i < count; i++)
+    parent_args.arr[i] = -1;
 
   pthread_t parent;
 
